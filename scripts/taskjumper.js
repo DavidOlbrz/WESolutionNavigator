67d3c90d9a887cb11fb363e8bc6a9090855d3360
David (_The_Agent_) <davidolbertz@gmail.com>
2024-01-07 18:10:01 +0100

    Fixed path

--- scripts/taskjumper.js
+++ scripts/taskjumper.js
@@ -1,6 +1,6 @@
 function jumpToTask(taskNo) {
     if (taskNo !== '0') {
         const number = taskNo < 10 ? '0' + taskNo : '' + taskNo;
-        location.href = 'uebung' + number + '/index.html';
+        location.href = '/uebung' + number + '/index.html';
     }
 }
