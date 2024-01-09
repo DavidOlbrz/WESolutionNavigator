ffa2d27f6c409e4dcb7bb1f86d91b55549ec408b
David (_The_Agent_) <davidolbertz@gmail.com>
2024-01-09 20:36:33 +0100

    Added header to all pages and fixed paths inside header

--- scripts/taskjumper.js
+++ scripts/taskjumper.js
@@ -1,6 +1,13 @@
 function jumpToTask(taskNo) {
     if (taskNo !== '0') {
         const number = taskNo < 10 ? '0' + taskNo : '' + taskNo;
-        location.href = '/uebung' + number + '/index.html';
+        location.href = '../uebung' + number;
+    }
+}
+
+function jumpToTaskFromHome(taskNo) {
+    if (taskNo !== '0') {
+        const number = taskNo < 10 ? '0' + taskNo : '' + taskNo;
+        location.href = 'uebung' + number;
     }
 }
